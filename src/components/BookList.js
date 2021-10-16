51303f772c66ae2a8219074f13fc73b2295c8c8b
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-16 22:45:24 +0300

    refactor: BookList component revised.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,6 +3,9 @@ import React from 'react';
 // Components
 import Book from './Book';
 
+// Styles
+import './BookList.css'
+
 class BookList extends React.Component {
     render() {
 
@@ -10,7 +13,21 @@ class BookList extends React.Component {
         return <Book book={book} key={i}/>
     })
    
-        return <div>{bookList}</div>
+        return (
+            <section className="page-section bg-light" id="portfolio">
+                <div className="container">
+                    <div className="text-center">
+                        <h2 className="section-heading text-uppercase">BookFolio</h2>
+                        <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    </div>
+                    <div className="row">
+
+                        {bookList}
+
+                    </div>
+                </div>
+            </section>
+        )
     }
 }
 export default BookList;
