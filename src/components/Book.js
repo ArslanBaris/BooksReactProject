6d4a43f457b0088fbe78dddb5f44089e5f04d8f9
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-16 22:30:41 +0300

    feat: Book component added.

---
+++ src/components/Book.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Book extends React.Component {
+  render() {
+    return <div>
+      <h2>{this.props.book.title}</h2>
+      <h4>{this.props.book.author}</h4>
+    </div>
+  }
+}
+export default Book;
